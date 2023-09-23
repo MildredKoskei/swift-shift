f02235fff72fc6fe7ab703740c335e20866f4e8d
Mildred Koskei <mildredkoskei01@gmail.com>
2023-09-23 16:31:26 +0300

    updated code files

---
+++ backend/models/contactSubmission.js
@@ -0,0 +1,36 @@
+// models/contactSubmission.js
+
+const mongoose = require('mongoose');
+
+const contactSubmissionSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+  },
+  industry: {
+    type: String,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const ContactSubmission = mongoose.model('ContactSubmission', contactSubmissionSchema);
+
+module.exports = ContactSubmission;
